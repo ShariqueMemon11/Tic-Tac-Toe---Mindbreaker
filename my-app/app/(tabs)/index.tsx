f390ce1a1ea93a921d013484b878ecb1ea27a223
ShariqMemon11 <memonshariq10@gmail.com>
2024-12-13 01:44:08 +0500

    created the app and done initial settings

---
+++ my-app/app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    color: '#000',
+  },
+});
